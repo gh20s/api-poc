38f4396e45ddc5b25558a2cf366e14957d504f34
gh20s <gregory.waters@gov.bc.ca>
2020-01-16 11:15:25 -0800

    added model.json + added complaint type

---
+++ 311v1.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+import json
+from enum import Enum
+
+# class to resemble a scikit-learn model using lookup date
+# allows median # of requests
+
+
+class naive_model:
+    data = None
+
+    def __init__(self, path='data/model.json'):
+        with open(path, 'r') as f:
+            self.data = json.load(f)
+
+    def predict(self, type_):
+        return self.data.get(type_, None)
+
+# create data validation for complaints
+
+
+class ComplaintType(str, Enum):
+    other = 'other'
+    commercial = 'commercial'
+    park = 'park'
+    residential = 'residential'
+    street = 'street'
+    vehicle = 'vehicle'
+    worship = 'worship'
+    truck = 'truck'
+
+
+app = FastAPI()
+model = naive_model()
+
+# passing 2 arguments, complaint is defined in the route provided above
+# hour is not specified since fastapi assumes it will be provided as a paramter
+# if you pass something without an hour, scheme validation issue
+@app.get('/complaints/noise/{complaint_type}/time')
+# adding complaint type astype hint
+def complaints(complaint_type: ComplaintType):
+    if complaint_type == ComplaintType.other:
+        ct = 'noise'
+    else:
+        ct = f'noise - {complaint_type.value}'
+    return {
+        'complaint_type': complaint_type,
+        'ct': ct,
+        'expectied_time': model.predict(ct),
+    }
